3240ca13f3fe9496c4d12c882e8f9a61b5582643
ritzle <vladislav.ryazanov.study@gmail.com>
2025-04-23 02:24:30 +0700

    обновление модели

--- file-backup-system-backend/app.py
+++ file-backup-system-backend/app.py
@@ -2,18 +2,48 @@ from flask import Flask
 from flask_cors import CORS 
 from auth import auth_bp 
 from HomePage import HomePage_bp
+from ServerDetail import ServerDetail_bp
 from config import Config
 from models import db
 
+import logging
+from colorlog import ColoredFormatter
+
+# Убрать дублирование логов от werkzeug
+logging.getLogger('werkzeug').propagate = False
+
+# Цветной формат логов
+formatter = ColoredFormatter(
+    "%(log_color)s[%(levelname)s] %(asctime)s - %(message)s",
+    datefmt="%H:%M:%S",
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_red'
+    }
+)
+
+# Потоковый handler
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+
+# Главный логгер
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(console_handler)
+
+# Инициализация Flask
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db.init_app(app)
-CORS(app)  
+CORS(app)
 
 app.register_blueprint(auth_bp)
 app.register_blueprint(HomePage_bp)
+app.register_blueprint(ServerDetail_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
--- file-backup-system-backend/models.py
+++ file-backup-system-backend/models.py
@@ -3,12 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
-
-    __tablename__ = 'users'
-
     
+    __tablename__ = "users"
+
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100), nullable=False)
-    last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(64))
+    last_name = db.Column(db.String(64))
+    password = db.Column(db.String(128))
+
+    servers = db.relationship("Server", backref="user", cascade="all, delete", lazy=True)
+
+class Server(db.Model):
+    __tablename__ = "servers"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    ip_address = db.Column(db.String(100), nullable=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
